4443eade1a158ad258656466cbb3da94ede9e24e
Harrison Bond <baronvonbadguy@gmail.com>
2018-06-15 19:44:01 -0400

    Redis client and Telegram bot now global scope

--- commands.go
+++ commands.go
@@ -4,30 +4,29 @@ import (
 	"log"
 	"strings"
 	"fmt"
-	"github.com/go-redis/redis"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"time"
 )
 
-func begin(client *redis.Client, b *tb.Bot, m *tb.Message, p Path) {
+func begin(m *tb.Message, p Path) {
 	key := fmt.Sprintf("%d:activePath", m.Sender.ID)
-	err := client.Del(key).Err()
+	err := R.Del(key).Err()
 	if err != nil {
 		log.Printf("unable to delete active path for %d: %s", m.Sender.ID, err)
 		return
 	}
-	err = client.Set(key, &p, time.Minute).Err()
+	err = R.Set(key, &p, time.Minute).Err()
 	if err != nil {
 		log.Printf("unable to set active path for %d: %s", m.Sender.ID, err)
 		return
 	}
 	m.Text = ""
-	step(client, b, m)
+	step(m)
 }
 
-func step(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func step(m *tb.Message) {
 	key := fmt.Sprintf("%d:activePath", m.Sender.ID)
-	pathData, err := client.Get(key).Result()
+	pathData, err := R.Get(key).Result()
 	if err != nil {
 		log.Printf("attempting to prompt but user %d has no active path: %s", m.Sender.ID, err)
 		return
@@ -48,37 +47,37 @@ func step(client *redis.Client, b *tb.Bot, m *tb.Message) {
 		} else {
 			log.Printf("function %s cannot be found", p.Command)
 		}
-		_, err := client.Del(key).Result()
+		_, err := R.Del(key).Result()
 		if err != nil {
 			log.Printf("unable to delete active path for user %d: %s", m.Sender.ID, err)
 		}
 		return
 	}
-	b.Send(m.Sender, fmt.Sprint(p.Prompts[p.Index]))
+	B.Send(m.Sender, fmt.Sprint(p.Prompts[p.Index]))
 	p.Index += 1
-	client.Set(key, &p, time.Minute)
+	R.Set(key, &p, time.Minute)
 }
 
-func registerStaticCommand(client *redis.Client, b *tb.Bot, userID int, name string, text string) (error) {
-	b.Handle(name, func(m *tb.Message){
-		b.Send(m.Sender, text)
+func registerStaticCommand(userID int, name string, text string) (error) {
+	B.Handle(name, func(m *tb.Message) {
+		B.Send(m.Sender, text)
 	})
-	channel, _ := getUsersActiveChannel(client, userID)
+	channel, _ := getUsersActiveChannel(userID)
 	hashKey := fmt.Sprintf("%s:commands", channel)
-	err := client.HSet(hashKey, name, text).Err()
+	err := R.HSet(hashKey, name, text).Err()
 	return err
 }
 
-func unregisterStaticCommand(client *redis.Client, b *tb.Bot, userID int, name string) (error) {
-	b.Handle(name, func(m *tb.Message){})
-	channel, _ := getUsersActiveChannel(client, userID)
+func unregisterStaticCommand(userID int, name string) (error) {
+	B.Handle(name, func(m *tb.Message) {})
+	channel, _ := getUsersActiveChannel(userID)
 	hashKey := fmt.Sprintf("%s:commands", channel)
-	err := client.HDel(hashKey, name).Err()
+	err := R.HDel(hashKey, name).Err()
 	return err
 }
 
-func getUsersActiveChannel(client *redis.Client, userID int) (string, error) {
-	activeChannel, err := client.Get(fmt.Sprintf("activeChannel:%d", userID)).Result()
+func getUsersActiveChannel(userID int) (string, error) {
+	activeChannel, err := R.Get(fmt.Sprintf("activeChannel:%d", userID)).Result()
 	if err != nil {
 		log.Printf("userID: %d doesn't have an active channel but tried to access it", userID)
 		return "", err
@@ -86,74 +85,74 @@ func getUsersActiveChannel(client *redis.Client, userID int) (string, error) {
 	return activeChannel, nil
 }
 
-func getKeyboardForCommands(commands map[string]func(m *tb.Message), b *tb.Bot) ([][]tb.ReplyButton) {
+func getKeyboardForCommands(commands map[string]func(m *tb.Message)) ([][]tb.ReplyButton) {
 	replyKeys := [][]tb.ReplyButton{}
 	replyRow := []tb.ReplyButton{}
 	for k, v := range commands {
 		replyBtn := tb.ReplyButton{Text: k}
-		b.Handle(&replyBtn, v)
+		B.Handle(&replyBtn, v)
 		replyRow = append(replyRow, replyBtn)
 	}
 	replyKeys = append(replyKeys, replyRow)
 	return replyKeys
 }
 
-func listCommandFunctions(client *redis.Client, b *tb.Bot, m *tb.Message) {
-	b.Send(m.Sender, "Here are the command management commands", &tb.ReplyMarkup{
-		ReplyKeyboard: getKeyboardForCommands(CommandFunctions, b),
+func listCommandFunctions(m *tb.Message) {
+	B.Send(m.Sender, "Here are the command management commands", &tb.ReplyMarkup{
+		ReplyKeyboard: getKeyboardForCommands(CommandFunctions),
 	})
 }
 
-func listAdminFunctions(client *redis.Client, b *tb.Bot, m *tb.Message) {
-	b.Send(m.Sender, "Here are the admin management commands", &tb.ReplyMarkup{
-		ReplyKeyboard: getKeyboardForCommands(AdminFunctions, b),
+func listAdminFunctions(m *tb.Message) {
+	B.Send(m.Sender, "Here are the admin management commands", &tb.ReplyMarkup{
+		ReplyKeyboard: getKeyboardForCommands(AdminFunctions),
 	})
 }
 
-func listFunctionGroups(client *redis.Client, b *tb.Bot, m *tb.Message) {
-	b.Send(m.Sender, "Check out these commands!", &tb.ReplyMarkup{
-		ReplyKeyboard: getKeyboardForCommands(FunctionGroups, b),
+func listFunctionGroups(m *tb.Message) {
+	B.Send(m.Sender, "Check out these commands!", &tb.ReplyMarkup{
+		ReplyKeyboard: getKeyboardForCommands(FunctionGroups),
 	})
 }
 
-func switchChannel(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func switchChannel(m *tb.Message) {
 	split := strings.Split(strings.Replace(m.Text, "/switchChannel ", "", 1), " ")
 	channelName := split[0]
 	hKey := fmt.Sprintf("activeChannel:%d", m.Sender.ID)
-	client.Set(hKey, channelName, 0)
-	b.Send(m.Sender, fmt.Sprintf("switched to managing channel: %s", channelName))
+	R.Set(hKey, channelName, 0)
+	B.Send(m.Sender, fmt.Sprintf("switched to managing channel: %s", channelName))
 }
 
-func addAdmin(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func addAdmin(m *tb.Message) {
 	split := strings.Split(strings.Replace(m.Text, "/addAdmin ", "", 1), " ")
 	adminName := split[0]
-	channel, _ := getUsersActiveChannel(client, m.Sender.ID)
+	channel, _ := getUsersActiveChannel(m.Sender.ID)
 	setKey := fmt.Sprintf("%s:admins", channel)
-	client.SAdd(setKey, adminName)
-	val, _ := client.SMembers(setKey).Result()
-	b.Send(m.Sender, fmt.Sprintf("admins now %s", val))
-	client.Del(fmt.Sprintf("nextCommand:%d", m.Sender.ID))
+	R.SAdd(setKey, adminName)
+	val, _ := R.SMembers(setKey).Result()
+	B.Send(m.Sender, fmt.Sprintf("admins now %s", val))
+	R.Del(fmt.Sprintf("nextCommand:%d", m.Sender.ID))
 }
 
-func removeAdmin(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func removeAdmin(m *tb.Message) {
 	split := strings.Split(strings.Replace(m.Text, "/removeAdmin ", "", 1), " ")
 	adminName := split[0]
-	channel, _ := getUsersActiveChannel(client, m.Sender.ID)
+	channel, _ := getUsersActiveChannel(m.Sender.ID)
 	setKey := fmt.Sprintf("%s:admins", channel)
-	client.SRem(setKey, adminName)
-	val, _ := client.SMembers(setKey).Result()
-	b.Send(m.Sender, fmt.Sprintf("admins for channel %s now: %s", channel, val))
-	client.Del(fmt.Sprintf("nextCommand:%d", m.Sender.ID))
+	R.SRem(setKey, adminName)
+	val, _ := R.SMembers(setKey).Result()
+	B.Send(m.Sender, fmt.Sprintf("admins for channel %s now: %s", channel, val))
+	R.Del(fmt.Sprintf("nextCommand:%d", m.Sender.ID))
 }
 
-func listAdmins(client *redis.Client, b *tb.Bot, m *tb.Message) {
-	channel, _ := getUsersActiveChannel(client, m.Sender.ID)
+func listAdmins(m *tb.Message) {
+	channel, _ := getUsersActiveChannel(m.Sender.ID)
 	setKey := fmt.Sprintf("%s:admins", channel)
-	val, _ := client.SMembers(setKey).Result()
-	b.Send(m.Sender, fmt.Sprintf("admins for channel %s: %s", channel, val))
+	val, _ := R.SMembers(setKey).Result()
+	B.Send(m.Sender, fmt.Sprintf("admins for channel %s: %s", channel, val))
 }
 
-func addCommand(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func addCommand(m *tb.Message) {
 	log.Printf("entered addCommand with msg %s", m.Text)
 	split := strings.Split(strings.Replace(m.Text, "/addCommand ", "", 1), ";")
 	commandName := split[0]
@@ -162,30 +161,30 @@ func addCommand(client *redis.Client, b *tb.Bot, m *tb.Message) {
 	}
 	commandText := split[1]
 	if len(split) == 1 {
-		b.Send(m.Sender, fmt.Sprint(
+		B.Send(m.Sender, fmt.Sprint(
 			"you need to specify a command and response to add, such as /addCommand commandName;response text"))
 	}
-	if err := registerStaticCommand(client, b, m.Sender.ID, commandName, commandText); err != nil {
-		b.Send(m.Sender, fmt.Sprintf("error while trying to add command: %s", commandName))
+	if err := registerStaticCommand(m.Sender.ID, commandName, commandText); err != nil {
+		B.Send(m.Sender, fmt.Sprintf("error while trying to add command: %s", commandName))
 	}
-	b.Send(m.Sender, fmt.Sprintf("added/overwrote command: %s", commandName))
+	B.Send(m.Sender, fmt.Sprintf("added/overwrote command: %s", commandName))
 }
 
-func removeCommand(client *redis.Client, b *tb.Bot, m *tb.Message) {
+func removeCommand(m *tb.Message) {
 	log.Printf("entered removeCommand with msg %s", m.Text)
 	commandName := strings.Replace(m.Text, "/removeCommand ", "", 1)
 	if ! strings.HasPrefix(commandName, "/") {
 		commandName = "/" + commandName
 	}
-	if err := unregisterStaticCommand(client, b, m.Sender.ID, commandName); err != nil {
-		b.Send(m.Sender, fmt.Sprintf("error while trying to remove command: %s", commandName))
+	if err := unregisterStaticCommand(m.Sender.ID, commandName); err != nil {
+		B.Send(m.Sender, fmt.Sprintf("error while trying to remove command: %s", commandName))
 	}
-	b.Send(m.Sender, fmt.Sprintf("removed command: %s", commandName))
+	B.Send(m.Sender, fmt.Sprintf("removed command: %s", commandName))
 }
 
-func listCommands(client *redis.Client, b *tb.Bot, m *tb.Message) {
-	channel, _ := getUsersActiveChannel(client, m.Sender.ID)
+func listCommands(m *tb.Message) {
+	channel, _ := getUsersActiveChannel(m.Sender.ID)
 	key := fmt.Sprintf("%s:commands", channel)
-	val, _ := client.HKeys(key).Result()
-	b.Send(m.Sender, fmt.Sprintf("commands for channel %s: %s", channel, val))
+	val, _ := R.HKeys(key).Result()
+	B.Send(m.Sender, fmt.Sprintf("commands for channel %s: %s", channel, val))
 }
--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"os"
 	"fmt"
 	"encoding/json"
-	)
+)
 
-var FunctionGroups = map[string]func(m *tb.Message){}
-var Functions = map[string]func(m *tb.Message){}
-var AdminFunctions = map[string]func(m *tb.Message){}
-var CommandFunctions = map[string]func(m *tb.Message){}
+var FunctionGroups map[string]func(m *tb.Message)
+var Functions map[string]func(m *tb.Message)
+var AdminFunctions map[string]func(m *tb.Message)
+var CommandFunctions map[string]func(m *tb.Message)
+
+var R *redis.Client
+var B *tb.Bot
 
 type Path struct {
 	Prompts   []string `json:"prompts`
@@ -36,13 +39,13 @@ func (p *Path) UnmarshalBinary(data []byte) error {
 
 func main() {
 
-	client := redis.NewClient(&redis.Options{
+	R = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       15,
 	})
-
-	b, err := tb.NewBot(tb.Settings{
+	var err error
+	B, err = tb.NewBot(tb.Settings{
 		Token:  os.Getenv("TELEBOT_SECRET"),
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
@@ -52,15 +55,9 @@ func main() {
 		return
 	}
 
-	wrapFunction := func(callable func(client *redis.Client, b *tb.Bot, m *tb.Message)) func(m *tb.Message) {
-		return func(m *tb.Message) {
-			callable(client, b, m)
-		}
-	}
-
 	wrapPathBegin := func(p Path) func(m *tb.Message) {
 		return func(m *tb.Message) {
-			begin(client, b, m, p)
+			begin(m, p)
 		}
 	}
 	AdminFunctions = map[string]func(m *tb.Message){
@@ -72,7 +69,7 @@ func main() {
 			Prompts: []string{"Who would you like to remove as an admin?"},
 			Command: "/removeAdmin",
 		}),
-		"View Admins": wrapFunction(listAdmins),
+		"View Admins": listAdmins,
 	}
 	CommandFunctions = map[string]func(m *tb.Message){
 		"Add Command": wrapPathBegin(Path{
@@ -84,21 +81,21 @@ func main() {
 			Prompts: []string{"What command would you like to remove?"},
 			Command: "/removeCommand",
 		}),
-		"View Commands": wrapFunction(listCommands),
+		"View Commands": listCommands,
 	}
 	Functions = map[string]func(m *tb.Message){
-		"/addAdmin":           wrapFunction(addAdmin),
-		"/removeAdmin":        wrapFunction(removeAdmin),
-		"/viewAdmins":         wrapFunction(listAdmins),
-		"/listAdminFunctions": wrapFunction(listAdminFunctions),
-		"/switchChannel":      wrapFunction(switchChannel),
-		"/addCommand":         wrapFunction(addCommand),
-		"/removeCommand":      wrapFunction(removeCommand),
-		"/viewCommands":       wrapFunction(listCommands),
+		"/addAdmin":           addAdmin,
+		"/removeAdmin":        removeAdmin,
+		"/viewAdmins":         listAdmins,
+		"/listAdminFunctions": listAdminFunctions,
+		"/switchChannel":      switchChannel,
+		"/addCommand":         addCommand,
+		"/removeCommand":      removeCommand,
+		"/viewCommands":       listCommands,
 	}
 	FunctionGroups = map[string]func(m *tb.Message){
-		"Manage Commands": wrapFunction(listCommandFunctions),
-		"Manage Admins":   wrapFunction(listAdminFunctions),
+		"Manage Commands": listCommandFunctions,
+		"Manage Admins":   listAdminFunctions,
 		"Switch Channel": wrapPathBegin(Path{
 			Prompts: []string{"Who channel would you like to switch to?"},
 			Command: "/switchChannel",
@@ -106,27 +103,27 @@ func main() {
 	}
 
 	for k, v := range Functions {
-		b.Handle(k, v)
+		B.Handle(k, v)
 	}
 
 	// Command: /start <PAYLOAD>
-	b.Handle("/start", func(m *tb.Message) {
+	B.Handle("/start", func(m *tb.Message) {
 		if !m.Private() {
 			return
 		}
-		wrapFunction(listFunctionGroups)(m)
+		listFunctionGroups(m)
 	})
 
-	b.Handle(tb.OnText, func(m *tb.Message) {
+	B.Handle(tb.OnText, func(m *tb.Message) {
 		// step forward if user has an active path
 		key := fmt.Sprintf("%d:activePath", m.Sender.ID)
-		exists, err := client.Exists(key).Result()
+		exists, err := R.Exists(key).Result()
 		if err != nil {
 			log.Printf("error looking up existence of active path for %s", m.Sender.ID)
 			return
 		}
 		if exists != 0 {
-			step(client, b, m)
+			step(m)
 		}
 		// // check if added static command
 		// if strings.HasPrefix(m.Text, "/") {
@@ -140,14 +137,14 @@ func main() {
 
 	})
 
-	b.Start()
+	B.Start()
 
 	interrupt := make(chan os.Signal, 1)
 	select {
 	case <-interrupt:
 		log.Println("interrupt")
 
-		b.Stop()
+		B.Stop()
 		return
 	}
 }
